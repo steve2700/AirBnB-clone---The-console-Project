e36dc5b75b55c519ba271545841a318d10968a22
steve2700 <steve2700@github.com>
2023-02-09 03:18:38 -0800

    upda

---
+++ updated_models.py
@@ -0,0 +1,28 @@
+from models import storage
+
+class BaseModel:
+    def __init__(self, *args, **kwargs):
+        """Initialize the BaseModel class"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid1())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+
+    def __str__(self):
+        """Returns a string representation of the BaseModel instance"""
+        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary
+
